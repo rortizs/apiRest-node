7f91deb2e452d496ea29645cdf3b868739892755
Richard Ortiz <richardortiz@Richards-MacBook-Pro-2.local>
2023-05-24 11:35:10 -0600

    update procedure.sql

--- db/procedures.sql
+++ db/procedures.sql
@@ -1,24 +1,16 @@
-USE db;
-
-DELIMETER $$
-USE `db`$$
-
-CREATE PROCEDURE `employeeAddOrEdit`
-(
+CREATE DEFINER=`root`@`localhost` PROCEDURE `employeeAddOrEdit`(
     IN _id INT,
     IN _name VARCHAR(45),
     IN _salary INT
-
 )
 BEGIN
     IF _id = 0 THEN
-        INSERT INTO employee(name, salary) VALUES (_name, _salary);
+        INSERT INTO employee (name, salary) VALUES (_name, _salary);
         SET _id = LAST_INSERT_ID();
     ELSE
-        UPDATE employee SET 
-        name = _name, 
-        salary = _salary 
+        UPDATE employee 
+        SET name = _name, salary = _salary 
         WHERE id = _id;
     END IF;
     SELECT _id AS 'id';
-END$$
+END
